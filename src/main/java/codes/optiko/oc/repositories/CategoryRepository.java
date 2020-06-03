35fbb9deded594a40c4b7d23a79108ac59ac0e78
Sinisa Tesic <sinisa.tesic210@gmail.com>
2020-06-03 10:01:28 -0500

    saving changes before pulling

--- src/main/java/codes/optiko/oc/repositories/CategoryRepository.java
+++ src/main/java/codes/optiko/oc/repositories/CategoryRepository.java
@@ -1,7 +1,13 @@
 package codes.optiko.oc.repositories;
 
 import codes.optiko.oc.model.Category;
+import codes.optiko.oc.model.Response;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+    @Query(value = "select * from posts where post_id = ?1", nativeQuery = true)
+    List<Category> findByPostId(Long id);
 }
